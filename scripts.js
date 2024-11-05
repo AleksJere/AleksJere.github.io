b6a98b116ad7cf5b54ac9ddf3cc19cecea3852fa
AleksJere <130003125+AleksJere@users.noreply.github.com>
2024-11-04 22:39:10 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,7 +14,9 @@ document.addEventListener('DOMContentLoaded', function() {
             modal.style.display = "none";
         }
     }
-
+    function goBack() {
+    window.history.back();
+    }
     // Close the modal when clicking outside the modal content
     window.onclick = function(event) {
         const modals = document.querySelectorAll('.modal');
@@ -28,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Make functions globally accessible
     window.openModal = openModal;
     window.closeModal = closeModal;
-
+    window.goBack = goBack;
     // Attach event listeners individually for each button
     document.getElementById('showModalBtn1').onclick = function() { openModal('modal1'); };
     document.getElementById('showModalBtn2').onclick = function() { openModal('modal2'); };
